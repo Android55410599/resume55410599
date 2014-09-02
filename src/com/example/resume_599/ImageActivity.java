f1537215d2c8b758f8b56ad7a5a4e219b67c112b
Android55410599 <onwara6916@gmail.com>
2014-09-02 19:04:56 +0700

    onwara

---
+++ src/com/example/resume_599/ImageActivity.java
@@ -0,0 +1,13 @@
+package com.example.resume_599;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImageActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.image_onwara);
+	}
+}
